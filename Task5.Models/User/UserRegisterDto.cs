161b813ca16b325aa714d7e86d1219b174bd30cc
insane013 <69902120+insane013@users.noreply.github.com>
2025-10-04 03:52:55 +0500

    user name max length constarint

--- Task5.Models/User/UserRegisterDto.cs
+++ Task5.Models/User/UserRegisterDto.cs
@@ -6,13 +6,16 @@ public class UserRegisterDto
 {
     [Required]
     [EmailAddress]
+    [MaxLength(32, ErrorMessage = "Max length for email is 32 symbols")]
     public string Email { get; init; } = string.Empty;
 
     [Required]
     [MinLength(1)] // extract defaults
+    [MaxLength(32, ErrorMessage = "Max length for Name is 32 symbols")]
     public string UserName { get; init; } = string.Empty;
 
-    public string UserPosition { get; init; }  = string.Empty;
+    [MaxLength(64, ErrorMessage = "Max length for Name is 64 symbols")]
+    public string UserPosition { get; init; } = string.Empty;
 
     [Required]
     [MinLength(1)] // extract defaults
